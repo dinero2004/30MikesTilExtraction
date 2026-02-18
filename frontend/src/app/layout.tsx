0be20b8dca1ea13f364fadfae704f0cee685266f
Lazar <lazar.minkov@gmail.com>
2026-02-18 19:05:41 +0100

    fixed fonts globaly part 2

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,13 @@ import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 import { ReactNode } from "react";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const captureIt = localFont({
+  src: "../public/fonts/Capture_It.ttf",
+  display: "swap",
+  variable: "--font-capture",
+});
 
 export default async function RootLayout({
   children,
